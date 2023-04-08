f8cdee8db72f672fdf04bdd34baad30d1c426575
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-04-08 21:19:11 +0000

    Deploying to gh-pages from @ YuliiaSliepchenko/goit-js-hw-11@f472cce61f5fd374aed78baee893ffa0280d8ac3 🚀

--- index.3b2b2755.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={inputGet:document.querySelector("input"),buttonSubmit:document.querySelector("button")};t.buttonSubmit.addEventListener("click",(function(){t.inputGet.value;fetch("https://pixabay.com/api/").then((function(t){return t.json()})).then((function(t){return console.log(t)}))}))}();
-//# sourceMappingURL=index.3b2b2755.js.map
